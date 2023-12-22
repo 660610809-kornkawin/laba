2250e960f0d349276c8b644e2c2ff43fbf2bf69b
660610809-kornkawin <kornkawin_th@cmu.ac.th>
2023-12-22 16:01:53 +0700

    lab11-1-compledted

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+string grade(){
+    srand(time(0));
+    int x = rand()%8;
+    if( x == 0) return "A";
+    else if ( x == 1) return "B+";
+    else if ( x == 2) return "B";
+    else if ( x == 3) return "C+";
+    else if ( x == 4) return "C";
+    else if ( x == 5) return "D+";
+    else if ( x == 6) return "D";
+    else return "F";
+}
+
+
+
+int main(){
+    cout<<"Press Enter 3 times to reveal your future.\n";
+    cin.get();
+    cin.get();
+    cin.get();
+    cout<<"You will get "<<grade()<<" in this 261102.";
+    return 0;
+}
 
